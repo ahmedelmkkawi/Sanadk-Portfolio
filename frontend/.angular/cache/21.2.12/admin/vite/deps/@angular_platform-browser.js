ea4c763d2e1f7793d647cfea2e1975afa892a95e
ahmedelmkkawi <ahmedalmkawey@gmail.com>
2026-05-25 03:05:40 +0300

    Configure standalone backend branch

---
+++ frontend/.angular/cache/21.2.12/admin/vite/deps/@angular_platform-browser.js
@@ -0,0 +1,84 @@
+import {
+  By,
+  DomSanitizer,
+  DomSanitizerImpl,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerGesturesPlugin,
+  HammerModule,
+  HydrationFeatureKind,
+  Meta,
+  Title,
+  VERSION,
+  disableDebugTools,
+  enableDebugTools,
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+  withI18nSupport,
+  withIncrementalHydration,
+  withNoHttpTransferCache
+} from "./chunk-LG7IBJ4U.js";
+import "./chunk-M7VO3GOZ.js";
+import {
+  BrowserDomAdapter,
+  BrowserGetTestability,
+  BrowserModule,
+  DomEventsPlugin,
+  DomRendererFactory2,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  KeyEventsPlugin,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  SharedStylesHost,
+  bootstrapApplication,
+  createApplication,
+  platformBrowser,
+  provideProtractorTestingSupport
+} from "./chunk-EDOI7KYV.js";
+import "./chunk-W25TNEIA.js";
+import {
+  getDOM
+} from "./chunk-W5YLD2HM.js";
+import "./chunk-A7HD4W5G.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  BrowserModule,
+  By,
+  DomSanitizer,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerModule,
+  HydrationFeatureKind,
+  Meta,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  Title,
+  VERSION,
+  bootstrapApplication,
+  createApplication,
+  disableDebugTools,
+  enableDebugTools,
+  platformBrowser,
+  provideClientHydration,
+  provideProtractorTestingSupport,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+  withI18nSupport,
+  withIncrementalHydration,
+  withNoHttpTransferCache,
+  BrowserDomAdapter as ɵBrowserDomAdapter,
+  BrowserGetTestability as ɵBrowserGetTestability,
+  DomEventsPlugin as ɵDomEventsPlugin,
+  DomRendererFactory2 as ɵDomRendererFactory2,
+  DomSanitizerImpl as ɵDomSanitizerImpl,
+  HammerGesturesPlugin as ɵHammerGesturesPlugin,
+  KeyEventsPlugin as ɵKeyEventsPlugin,
+  SharedStylesHost as ɵSharedStylesHost,
+  getDOM as ɵgetDOM
+};
--- frontend/libs/shared/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-export * from './services/auth.service';
-export * from './services/portfolio.service';
-export * from './services/auth.guard';
-export * from './services/auth.interceptor';
-export * from './models/portfolio.models';
